8c1c4c0d115af0f86e5f9a1538da801c478ce274
Simran <bhatnagarsimran1306@gmail.com>
2024-08-14 23:46:53 +0530

    added flask_cors

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import openai
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*", "methods": ["GET", "POST"]}})
 
 load_dotenv() #loads the .env file
 
